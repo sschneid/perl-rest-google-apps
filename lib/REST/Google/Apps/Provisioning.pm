2807a44947dda92caa059f189223e3c87777c535
Scott Schneider <sschneid@gmail.com>
2009-09-17 12:04:56 -0700

    Adding deleteGroupMember function

--- lib/REST/Google/Apps/Provisioning.pm
+++ lib/REST/Google/Apps/Provisioning.pm
@@ -400,7 +400,20 @@ sub addGroupMember {
 }
 
 sub deleteGroupMember {
-    # Not yet implemented
+    my $self = shift;
+
+    my ( $arg );
+    %{$arg} = @_;
+
+    foreach my $param ( qw/ group member / ) {
+        $arg->{$param} || croak( "Missing required '$param' argument" );
+    }
+
+    my $url = qq(https://apps-apis.google.com/a/feeds/group/2.0/$self->{'domain'}/$arg->{'group'}/member/$arg->{'member'});
+
+    my $result = $self->_request( 'method' => 'DELETE', 'url' => $url ) || return( 0 );
+
+    return( 1 ) if $result;
 }
 
 sub getGroupMember {
